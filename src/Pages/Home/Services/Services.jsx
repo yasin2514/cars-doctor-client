2d42bb27fd9ac2aee5a1366cd5cfd90d33eb3c82
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-05-29 11:50:20 +0600

    search and sort added

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,13 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [asc, setAsc] = useState(true);
+    const searchRef = useRef();
+    const [search, setSearch] = useState('');
     useEffect(() => {
-        fetch('https://car-doctor-server-lake-eta.vercel.app/services')
+        fetch(`http://localhost:5000/services?search=${search}&sort=${asc ? "asc" : "dsc"}`)
             .then(res => res.json())
             .then(data => setServices(data))
-    }, [])
+    }, [asc, search])
+
+    const handleSearch = () => {
+        setSearch(searchRef.current.value)
+    }
     return (
         <div className="mb-36">
             <div className="text-center pt-32 space-y-3 ">
@@ -15,6 +22,16 @@ const Services = () => {
                 <h1 className="text-5xl font-bold">Our Service Area</h1>
                 <p className="">the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable.
                 </p>
+                <button className="btn btn-sm" onClick={() => setAsc(!asc)}>{asc ? "Price High to Low" : "Price Low to High"}</button>
+                {/* search bar */}
+                <div className="form-control">
+                    <div className="input-group">
+                        <input type="text" ref={searchRef} placeholder="Search…" className="input input-bordered" />
+                        <button onClick={handleSearch} className="btn btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
             </div>
             <div className="grid grid-cols-3 gap-10 mt-10">
                 {
